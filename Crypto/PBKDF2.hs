de069bb65bc0dfba969bf51ccb9206f2fd18c3eb
Erik Hesselink <hesselink@gmail.com>
2013-09-17 16:32:38 +0200

    Fix more warnings.
    Added type signatures for defaulting, commented in type signatures, that were already there, removed unused variables.

--- Crypto/PBKDF2.hs
+++ Crypto/PBKDF2.hs
@@ -57,21 +57,21 @@ cIters: Number of iterations of prf
 dklen: Length of the derived key (hashed password)
 -}
 pbkdf2' :: ( ([Word8] -> [Word8] -> [Word8]),Integer) -> Integer -> Integer -> Password -> Salt -> HashedPass
-pbkdf2' (prf,hlen) cIters dklen (Password pass) (Salt salt) 
-  | dklen > ( (2^32-1) * hlen) = error $ "pbkdf2, (dklen,hlen) : " ++ (show (dklen,hlen))
-  | otherwise = 
-    let --l,r :: Int
-        l = ceiling $ (fromIntegral dklen) / (fromIntegral hlen )
-        r = dklen - ( (l-1) * hlen)
+pbkdf2' (prf,hlen) cIters dklen (Password pass) (Salt salt)
+  | dklen > ( (2^(32 :: Integer)-1) * hlen) = error $ "pbkdf2, (dklen,hlen) : " ++ (show (dklen,hlen))
+  | otherwise =
+    let l :: Integer
+        l = ceiling $ (fromIntegral dklen :: Double) / (fromIntegral hlen )
+        -- r = dklen - ( (l-1) * hlen)
         ustream :: [Word8] -> [Word8] -> [[Word8]]
         ustream p s = let x = prf p s
-                      in  x : ustream p x    
-        --us :: Integer -> [[Word8]]
+                      in  x : ustream p x
+        us :: Integer -> [[Word8]]
         us i = take (fromIntegral cIters) $ ustream pass ( salt `myor` ((intToFourWord8s i) ))
-        --f :: [Word8] -> [Word8] -> Integer -> Integer -> [Word8]
-        f pass salt cIters i = foldr1 myxor $ us i
+        f :: [Word8] -> [Word8] -> Integer -> Integer -> [Word8]
+        f i = foldr1 myxor $ us i
         ts :: [[Word8]]
-        ts = map (f pass salt cIters) ( [1..l] )
+        ts = map f ( [1..l] )
     in HashedPass . take (fromIntegral dklen) . concat $ ts
 
 toOctets :: (Binary a) => a -> [Word8]
@@ -83,7 +83,7 @@ fromOctets = decode . L.pack
 -- The spec says
 -- Here, INT (i) is a four-octet encoding of the integer i, most significant octet first.
 -- I'm reading from the right, which I think is the right thing.
---intToFourWord8s :: Integer -> [Word8]
+intToFourWord8s :: Integer -> [Word8]
 intToFourWord8s i = let w8s =  toOctets $ i
                     in drop (length w8s -4) w8s
 
